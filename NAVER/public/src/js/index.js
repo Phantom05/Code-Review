c6419fba0aeaf2e04026f261ce33ecb4db864416
phantom05 <monster2jy@gmail.com>
2018-06-22 21:04:08 +0900

    docs:Edit NAVER ie7 project to includes Make middle part and iframe js

--- NAVER/public/src/js/index.js
+++ NAVER/public/src/js/index.js
@@ -6,8 +6,10 @@
 // nav more start
 const NAV_MORE=document.getElementById('navMore');
 const NAV_OPA=document.getElementById('navOpacity');
+const NAV_LIST=document.getElementById('navMoreUl');
 NAV_MORE.addEventListener('click',function(e){
-  e.target.firstElementChild.classList.toggle('active');
+  // e.target.firstElementChild.classList.toggle('active');
+  NAV_LIST.classList.toggle('active');
   NAV_OPA.classList.toggle('active');
 })
 // nav more end
